10d1d05c2fc92fe5004d75de74b98aaa953a963a
NikaGolestani <83786259+NikaGolestani@users.noreply.github.com>
2022-10-25 15:20:04 +0300

    Create isprimary.py

---
+++ isprimary.py
@@ -0,0 +1,27 @@
+while True:
+    num=int(input('num='))
+    if num>1:
+        arr=[]
+        primaryarr=[]
+        for i in range(2,round((num/2)+1)):
+            if num%i==0:
+                arr.append(i)
+            lenarr=len(arr)
+            while lenarr>0:
+                for x in arr:
+                    for y in arr:
+                        if y==x:
+                            continue
+                        elif y % x == 0:
+                            arr.remove(y)
+
+                lenarr-=1
+
+        if len(arr)!=0:
+            print(f'{num} is not a primary number \n {num}:{arr}')
+        else:
+            print(f'{num} is a primary number')
+    elif num<0:
+        print(f'{num} is an invalid input')
+    else:
+        print(f'{num} is not a primary number')
