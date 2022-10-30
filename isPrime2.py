1b75bd4237c7b00434f0a8ada11bba5fe311ccbf
Nika Golestani <ng8224@gmail.com>
2022-10-30 20:30:48 +0300

    debugg square numbers

--- isPrime2.py
+++ isPrime2.py
@@ -1,29 +1,25 @@
 num=input("number=")
 x=int(num)
 arr=[]
-firstprimes=[2,3,5,7,11,13,17,19,23]
 if x>1:
- for i in firstprimes :
-  if x%i==0:
-   arr.append(i)
-   x=int(x/i)
  while x!=1:
-  for i in range(2,x+1):
+
+  for i in range(2,int(x+1)):
    if x%i==0:
     if i not in arr:
      arr.append (i)
     x/=i
+    break
  for b in arr:
   for a in arr:
-   if b%a==0 and a!=b:
+   if len(arr) > 1 and b%a==0 and a!=b:
     arr.remove(b)
     break
- if len(arr)==1:
+ if len(arr)==1 and arr[0] == int(num):
   print (f"{num} is a Prime Number")
- elif len(arr)>1:
+ elif len(arr)>0:
   print (f"{num} is not a Prime Number.{arr}")
 elif x>=0:
  print(f"{num} is not a Prime Number.")
 else:
  print("invalid input")
- 
