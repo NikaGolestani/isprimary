2dabf96a51a61847df7ee6bf9b73a444e3de472d
NikaGolestani <83786259+NikaGolestani@users.noreply.github.com>
2022-11-08 13:46:36 +0300

    Add vomments to prime2.py

--- isPrime2.py
+++ isPrime2.py
@@ -1,24 +1,34 @@
+#manage inputs
 num=input("number=")
 x=int(num)
 arr=[]
+#define the range
 if x>1:
  while x!=1:
-
-  for i in range(2,int(x+1)):
+#convert even to odd
+  if x%2==0:
+   arr.append(2)
+   while x%2==0:
+    x//=2
+#find odd components
+  for i in range(3,x+1,2):
    if x%i==0:
     if i not in arr:
      arr.append (i)
-    x/=i
+    x=x//i
     break
+#eliminate NOT prime numbers
  for b in arr:
   for a in arr:
    if len(arr) > 1 and b%a==0 and a!=b:
     arr.remove(b)
     break
+#print the result
  if len(arr)==1 and arr[0] == int(num):
   print (f"{num} is a Prime Number")
  elif len(arr)>0:
   print (f"{num} is not a Prime Number.{arr}")
+#not in range 2->infinity
 elif x>=0:
  print(f"{num} is not a Prime Number.")
 else:
